94ec2ee05334de12494d42c6918052a6487984f4
Andrew <54529269+Andrew9614@users.noreply.github.com>
2023-02-11 20:58:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,37 +3,156 @@ const fileInput = document.querySelector('.file-input'),
   filterName = document.querySelector('.filter-info .name'),
   filterValue = document.querySelector('.filter-info .value'),
   filterSlider = document.querySelector('.slider input'),
-  rotateOptions = document.querySelectorAll('.rotate button'),
-  previewImg = document.querySelector('.target-img'),
+  targetImg = document.querySelector('.target-img'),
   frameImg = document.querySelector('.frame-img'),
   resetFilterBtn = document.querySelector('.reset-filter'),
   chooseImgBtn = document.querySelector('.choose-img'),
-  saveImgBtn = document.querySelector('.save-img');
+  saveImgBtn = document.querySelector('.save-img'),
+  textInput = document.querySelector('.text-input'),
+  renderText = document.querySelector('.render-text'),
+  container = document.querySelector('.preview-img').getBoundingClientRect();
 
-let isDragging = false,
+let frameImgRec = frameImg.getBoundingClientRect(),
+  isDragging = false,
   currentX,
   currentY,
   initialX,
   initialY,
-  xOffset = 0,
-  yOffset = 0;
+  scale = 100,
+  rotate = 0,
+  textSize = frameImgRec.height * 0.04,
+  textLeft = frameImgRec.width * 0.1,
+  textTop = frameImgRec.height * 0.8;
 
-previewImg.style.top = 500 / 2 - previewImg.height / 2 + 'px';
-
-let scale = 100,
-  rotate = 0;
 const loadImage = () => {
   let file = fileInput.files[0];
   if (!file) return;
-  previewImg.src = URL.createObjectURL(file);
-  previewImg.addEventListener('load', () => {
+  targetImg.src = URL.createObjectURL(file);
+  targetImg.addEventListener('load', () => {
     resetFilterBtn.click();
     document.querySelector('.container').classList.remove('disable');
   });
 };
 
 const applyFilter = () => {
-  previewImg.style.transform = `rotate(${rotate}deg) scale(${scale / 100})`;
+  targetImg.style.transform = `rotate(${rotate}deg) scale(${scale / 100})`;
+};
+
+const updateFilter = () => {
+  filterValue.innerText = `${filterSlider.value}%`;
+  const selectedFilter = document.querySelector('.filter .active');
+
+  if (selectedFilter.id === 'scale') {
+    scale = filterSlider.value;
+  } else if (selectedFilter.id === 'rotate') {
+    rotate = filterSlider.value;
+  } else if (selectedFilter.id === 'inversion') {
+    inversion = filterSlider.value;
+  } else {
+    grayscale = filterSlider.value;
+  }
+  applyFilter();
+};
+
+const resetFilter = () => {
+  rotate = 0;
+  scale = 100;
+  setTranslate(0, frameImg.height / 2 - targetImg.height / 2, targetImg);
+  xOffset = 0;
+  yOffset = frameImg.height / 2 - targetImg.height / 2;
+  filterOptions[0].click();
+  textInput.value = '';
+  renderText.innerHTML = '';
+  applyFilter();
+};
+
+const saveImage = () => {
+  console.log(frameImg.naturalWidth);
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+  let upscale = frameImg.naturalWidth/frameImg.width;
+  canvas.width = container.width * upscale;
+  canvas.height = container.height * upscale;
+  let targetLeft = targetImg.offsetLeft * upscale;
+  let targetTop = targetImg.offsetTop * upscale;
+  let targetWidth = (targetImg.width * scale * upscale) / 100;
+  let targetHeight = (targetImg.height * scale * upscale) / 100;
+  let targetCenterX = targetLeft + (targetImg.width * upscale) / 2;
+  let targetCenterY = targetTop + (targetImg.height * upscale) / 2;
+
+  if (scale / 100 !== 1) {
+    targetLeft = targetLeft - (targetWidth - targetImg.width) / 2;
+    targetTop = targetTop - (targetHeight - targetImg.height) / 2;
+  }
+
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.save();
+  ctx.translate(targetCenterX, targetCenterY);
+  if (rotate !== 0) ctx.rotate((rotate * Math.PI) / 180);
+  ctx.drawImage(
+    targetImg,
+    -targetWidth / 2,
+    -targetHeight / 2,
+    targetWidth,
+    targetHeight
+  );
+  ctx.restore();
+  ctx.drawImage(frameImg, 0, 0, canvas.width, canvas.height);
+  ctx.font = `${textSize * upscale}px SansSerif`;
+  ctx.fillStyle = 'black';
+  ctx.fillText(
+    renderText.innerHTML,
+    textLeft * upscale,
+    (textTop + textSize) * upscale
+  );
+  // document.querySelector('.container').append(canvas);
+  const link = document.createElement('a');
+  link.download = 'image.jpg';
+  link.href = canvas.toDataURL();
+  link.click();
+};
+
+const frameResize = () => {
+  frameImgRec = frameImg.getBoundingClientRect();
+  textSize = frameImgRec.height * 0.04;
+  textLeft = frameImgRec.width * 0.1;
+  textTop = frameImgRec.height * 0.8;
+  renderText.style.fontSize = textSize + 'px';
+  renderText.style.top = textTop + 'px';
+  renderText.style.left = textLeft + 'px';
+};
+
+const handleInputChange = (e) => {
+  renderText.innerHTML = e.target.value ? `...${e.target.value}` : '';
+};
+
+const dragStart = (e) => {
+  e.preventDefault();
+  initialX = e.clientX - e.target.offsetLeft;
+  initialY = e.clientY - e.target.offsetTop;
+  isDragging = true;
+};
+
+const dragEnd = () => {
+  isDragging = false;
+};
+
+const drag = (e) => {
+  if (e.target !== targetImg) return;
+  if (isDragging) {
+    e.preventDefault();
+    currentX = e.clientX - initialX;
+    currentY = e.clientY - initialY;
+
+    setTranslate(currentX, currentY, targetImg);
+  }
+};
+
+const setTranslate = (xPos, yPos, el) => {
+  el.style.left = xPos + 'px';
+  el.style.top = yPos + 'px';
 };
 
 filterOptions.forEach((option) => {
@@ -55,117 +174,20 @@ filterOptions.forEach((option) => {
   });
 });
 
-const updateFilter = () => {
-  filterValue.innerText = `${filterSlider.value}%`;
-  const selectedFilter = document.querySelector('.filter .active');
-
-  if (selectedFilter.id === 'scale') {
-    scale = filterSlider.value;
-  } else if (selectedFilter.id === 'rotate') {
-    rotate = filterSlider.value;
-  } else if (selectedFilter.id === 'inversion') {
-    inversion = filterSlider.value;
-  } else {
-    grayscale = filterSlider.value;
-  }
-  applyFilter();
-};
-
-rotateOptions.forEach((option) => {
-  option.addEventListener('click', () => {
-    if (option.id === 'left') {
-      rotate -= 90;
-    } else if (option.id === 'right') {
-      rotate += 90;
-    } else if (option.id === 'horizontal') {
-      flipHorizontal = flipHorizontal === 1 ? -1 : 1;
-    } else {
-      flipVertical = flipVertical === 1 ? -1 : 1;
-    }
-    applyFilter();
-  });
-});
-
-const resetFilter = () => {
-  rotate = 0;
-  scale = 100;
-  setTranslate(0, 0, previewImg);
-  xOffset = 0;
-  yOffset = 0;
-  filterOptions[0].click();
-  applyFilter();
-};
-
-const saveImage = () => {
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext('2d');
-  canvas.width = 333;
-  canvas.height = 500;
-  ctx.fillStyle = 'green';
-  ctx.fillRect(0, 0, 1000, 1000);
-  if (rotate !== 0) {
-    ctx.rotate((rotate * Math.PI) / 180);
-  }
-  //ctx.scale(scale / 100, scale / 100);
-  console.log(previewImg.offsetTop);
-  ctx.drawImage(
-    previewImg,
-    parseInt(previewImg.style.left),
-    parseInt(previewImg.style.top),
-    (previewImg.width * scale) / 100,
-    (previewImg.height * scale) / 100
-  );
-  ctx.drawImage(
-     frameImg,
-     0,
-     0,
-     frameImg.width,
-     frameImg.height
-   );
-
-  const link = document.createElement('a');
-  link.download = 'image.jpg';
-  link.href = canvas.toDataURL();
-  link.click();
-};
-
 filterSlider.addEventListener('input', updateFilter);
 resetFilterBtn.addEventListener('click', resetFilter);
 saveImgBtn.addEventListener('click', saveImage);
 fileInput.addEventListener('change', loadImage);
 chooseImgBtn.addEventListener('click', () => fileInput.click());
-
-previewImg.addEventListener('pointerdown', dragStart);
+textInput.addEventListener('input', handleInputChange);
+targetImg.addEventListener('pointerdown', dragStart);
 document.addEventListener('pointerup', dragEnd);
 document.addEventListener('pointerleave', dragEnd);
 document.addEventListener('pointermove', drag);
-
-function dragStart(e) {
-  e.preventDefault();
-  initialX = e.clientX - xOffset;
-  initialY = e.clientY - yOffset;
-
-  isDragging = true;
-}
-
-function dragEnd(e) {
-  isDragging = false;
-}
-
-function drag(e) {
-  if (isDragging) {
-    e.preventDefault();
-    currentX = e.clientX - initialX;
-    currentY = e.clientY - initialY;
-		console.log(previewImg.offsetTop)
-    xOffset = currentX;
-    yOffset = currentY;
-
-    setTranslate(currentX, currentY, previewImg);
-  }
-}
-
-function setTranslate(xPos, yPos, el) {
-  el.style.left = xPos + 'px';
-  el.style.top = yPos + 'px';
-}
+new ResizeObserver(frameResize).observe(frameImg);
+document.addEventListener('DOMContentLoaded', () => {
+  renderText.style.fontSize = textSize + 'px';
+  renderText.style.top = textTop + 'px';
+  renderText.style.left = textLeft + 'px';
+  setTranslate(0, frameImg.height / 2 - targetImg.height / 2, targetImg);
+});
